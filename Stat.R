b7e8997f620a74f1b615390209ed829464cfd238
kalpesh <kchaudhari686@gmail.com>
2017-11-30 04:09:53 +0530

    First version

--- Stat.R
+++ Stat.R
@@ -12,15 +12,3 @@ get_mode<-function(data){
   uniq<-unique(data)
   uniq[which.max(tabulate(match(data,uniq)))]
 }
-
-z_score<-function(data,user){
-  (user-min(data))/sd(data)
-}
-
-min_max<-function(data,user,new_min,new_max){
-  ((user-min(data))/(max(data)-min(data)))*(new_max-new_min)+new_min
-}
-
-decimal<-function(user,scale1){
-  (user/scale1)
-}
